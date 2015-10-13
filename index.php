0ac812e5abe096f6513b993eb90ca5b8b39e8308
vl1813 <vitor_1813@hotmail.com>
2015-10-13 07:25:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
+<html>
+
 <head>
- </head>
- <body>
+
+</head>
+ 
+<body>
+ 
 <?php require ("variaveis.php")?> 
-		<form action="corrigir.php" method="post">
+		<form action="corrigir.php" method="get">
 <?php
+
 $contador=0;
+
 $n=0;
+
 foreach($all as $atual){ 
+
 $contador++;
+
 $n++;?>
 							
 <?php echo "Questão ".$n.": ".$atual["enunciado"]."<br><br>";?>
 		
 	a)	<input type="radio" name="alternativa<?php echo $contador;?>" value="1"> 	<?php echo $atual["a"]."<br>"; ?>
 	b)	<input type="radio" name="alternativa<?php echo $contador;?>" value="2"> 	<?php echo $atual["b"]."<br>";?>
-c)	<input type="radio" name="alternativa<?php echo $contador;?>" value="3"> 	<?php echo $atual["c"]."<br><br>";}?>
+	c)	<input type="radio" name="alternativa<?php echo $contador;?>" value="3"> 	<?php echo $atual["c"]."<br><br>";}?>
  
  <input type="submit"  value="Corrigir" > 
 		</form>
