1f55712a54a442c0615371c7be9ada7c55559ced
vl1813 <vitor_1813@hotmail.com>
2015-10-13 07:25:21 -0300

    Update variaveis.php

--- variaveis.php
+++ variaveis.php
@@ -1,14 +1,20 @@
 <?php 
 
-$pergunta1 = array("enunciado" => "Segundo uma organização mundial de estudos ambientais em 2025, “ duas de cada três pessoas viverão situações de carência de água, caso não haja mudanças no padrão atual de consumo do produto”.
-Uma alternativa adequada e viável para prevenir a escassez, considerando-se a disponibilidade global, seria:", "a" => "Desenvolver processos de reutilização da água.", "b"=> " Explorar leitos de água subterrânea
-", "c"=> "Ampliar a oferta de água, captando-a em outros rios");
+$pergunta1 = array("enunciado" => "Qual a capital do brasil", "a" => "Haiti", "b"=> " Rio de Janeiro
+", "c"=> "Brasilia");
 
-$pergunta2 = array("enunciado"=>"Segundo uma organização mundial de estudos ambientais em 2025, “ duas de cada três pessoas viverão situações de carência de água, caso não haja mudanças no padrão atual de consumo do produto”.
-Uma alternativa adequada e viável para prevenir a escassez, considerando-se a disponibilidade global, seria:", "a"=>"Desenvolver processos de reutilização da água.", "b"=> " Explorar leitos de água subterrânea
-", "c"=> "Ampliar a oferta de água, captando-a em outros rios");
+$pergunta2 = array("enunciado"=>"Qual a capital da bosnia:?", "a"=>"viena", "b"=> "berlin", "c"=> "Sarajevo");
 
-$all = array($pergunta1, $pergunta2);
+$pergunta3 = array("enunciado"=>"Qual a cor que absorve todas as outras:", "a"=>"branco", "b"=> "magenta", "c"=> "preto");
 
-$altc = array("1"=>"1", "2"=>"3");
+$pergunta4 = array("enunciado"=>"Qual o movimento das luzes:", "a"=>"humanismo", "b"=> "hedonismo", "c"=> "iluminismo");
+
+$pergunta5 = array("enunciado"=>"Qual o seculo da revolucao francesa:", "a"=>"2", "b"=> "18", "c"=> "19");
+
+$pergunta6 = array("enunciado"=>"Qual o ano que a familia real veio ao Brasil:", "a"=>"1822", "b"=> "1808", "c"=> "2014");
+
+
+$all = array($pergunta1, $pergunta2,$pergunta3,$pergunta4,$pergunta5,$pergunta6);
+
+$altc = array(1=>"3", 2=>"3",3=>"3",4=>"3",5=>"2",6=>"2" );
 ?>
