ef4d441e60e9863193a7031cb01983a9ce7be0fd
vl1813 <vitor_1813@hotmail.com>
2015-10-12 21:17:35 -0300

    Create variaveis.php

---
+++ variaveis.php
@@ -0,0 +1,14 @@
+<?php 
+
+$pergunta1 = array("enunciado" => "Segundo uma organização mundial de estudos ambientais em 2025, “ duas de cada três pessoas viverão situações de carência de água, caso não haja mudanças no padrão atual de consumo do produto”.
+Uma alternativa adequada e viável para prevenir a escassez, considerando-se a disponibilidade global, seria:", "a" => "Desenvolver processos de reutilização da água.", "b"=> " Explorar leitos de água subterrânea
+", "c"=> "Ampliar a oferta de água, captando-a em outros rios");
+
+$pergunta2 = array("enunciado"=>"Segundo uma organização mundial de estudos ambientais em 2025, “ duas de cada três pessoas viverão situações de carência de água, caso não haja mudanças no padrão atual de consumo do produto”.
+Uma alternativa adequada e viável para prevenir a escassez, considerando-se a disponibilidade global, seria:", "a"=>"Desenvolver processos de reutilização da água.", "b"=> " Explorar leitos de água subterrânea
+", "c"=> "Ampliar a oferta de água, captando-a em outros rios");
+
+$all = array($pergunta1, $pergunta2);
+
+$altc = array("1"=>"1", "2"=>"3");
+?>
