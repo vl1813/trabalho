4efadc09fe5b33282aabca1db4f9b2d2b5433a3c
vl1813 <vitor_1813@hotmail.com>
2015-10-13 07:26:19 -0300

    Update corrigir.php

--- corrigir.php
+++ corrigir.php
@@ -3,11 +3,45 @@
 <?php 
 require("Variaveis.php");
 ?>
-<?php if($_POST["alternativa1"]=="1"){
+<?php 
+$n=1;
+
+	$correta=0;
 	
-	echo "1 correta."
+	$errada=0;
 	
+foreach( $altc as $a ){
+	$ind="alternativa".$n;
+if( $_GET[$ind]==$a){
+	
+	echo "Questao:".$n." Correta <br><br>";
+	$correta++;
 }
+else{
+	if($a=="1"){
+	
+	echo " Questao:".$n."Resposta errada. A correta e´: a <br><br>";
+	$errada++;
+	
+	}
+	if($a=="2"){
+	
+	echo " Questao:".$n." Resposta errada. A correta e´: b <br><br>";
+	$errada++;
+	
+	}
+	if($a=="3"){
+	
+	echo " Questao:".$n." Resposta errada. A correta e´: c <br><br>";
+	$errada++;
+	
+	}
+}
+
+$n++;
+
+}
+echo "<br>Voce errou ".$errada." e acertou ".$correta;
 ?>
 </body>
 </html>
